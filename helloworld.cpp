395f1cda0a11e26acff3cd381ed07fda3dfad79e
BioHazard <3482991796@qq.com>
2022-04-28 15:18:44 +0800

    initial

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    vector<int> s;
+    // cout << s[0];
+    cout << "Hello World!";
+    return 0;
+}
