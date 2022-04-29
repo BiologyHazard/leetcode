7b1223e24d0eb7c9abbcf1ccb6862e94fe28c238
MisakaVan <2102315149@qq.com>
2022-04-29 19:11:11 +0800

    Create 427-construct-quad-tree-MisakaVan.py
    MisakaVan‘s Solution

---
+++ 427-construct-quad-tree-MisakaVan.py
@@ -0,0 +1,26 @@
+# Definition for a QuadTree node.
+class Node:
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+
+
+class Solution:
+    def construct(self, grid: list[list[int]]) -> Node:
+        def subsearch(x1, y1, x2, y2):
+            if all(grid[x1][y1] == grid[i][j] for i in range(x1, x2) for j in range(y1, y2)):
+                return Node(grid[x1][y1], True, None, None, None, None)
+            else:
+                xm, ym = (x1+x2)//2, (y1+y2)//2
+                return Node(0, False,
+                            subsearch(x1, y1, xm, ym),
+                            subsearch(x1, ym, xm, y2),
+                            subsearch(xm, y1, x2, ym),
+                            subsearch(xm, ym, x2, y2))
+        x0 = len(grid)
+        tree = subsearch(0, 0, x0, x0)
+        return tree
