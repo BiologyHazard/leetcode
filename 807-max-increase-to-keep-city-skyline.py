b7fd545d4d92030b5e15b00fbfa474d6bd2bd965
BioHazard <3482991796@qq.com>
2022-04-30 11:58:04 +0800

    branch MisakaVan

--- 807-max-increase-to-keep-city-skyline.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-
-class Solution:
-    def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
-        a = [max(i) for i in grid]
-        b = [max(i) for i in zip(*grid)]
-        return sum(sum(min(a[i], b[j]) - grid[i][j] for j in range(len(grid[0]))) for i in range(len(grid)))
